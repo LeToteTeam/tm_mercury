435f81414614d2678cb76ff403a2e6117c3a94c8
Jeff Smith <jeff@letote.com>
2017-04-10 19:57:49 -0700

    Bump version to v0.2.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TM.Mercury.Mixfile do
 
   def project do
     [app: :tm_mercury,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.4.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
