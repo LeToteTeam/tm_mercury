d27ae41aecb2c84723a40367926a0908c9677ee5
Jeff Smith <jeff@letote.com>
2017-09-14 19:46:51 -0700

    Update ex_doc and earmark

--- mix.exs
+++ mix.exs
@@ -12,26 +12,14 @@ defmodule TM.Mercury.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [extra_applications: [:logger, :connection, :nerves_uart]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:connection, "~> 1.0.0"},
      {:nerves_uart, "~> 0.1"},
-     {:ex_doc, "~> 0.14", only: :dev, runtime: false}]
+     {:ex_doc, "~> 0.16.4", only: :dev, runtime: false}]
   end
 
   defp description do
