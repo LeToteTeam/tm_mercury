057ff469547652f4e6b211af006fdad68e053654
Jeff Smith <jeff@letote.com>
2017-10-11 15:22:07 -0700

    Get rid of unsafe variable warning in EPC parsing

--- lib/tm_mercury/tag/type/gen2.ex
+++ lib/tm_mercury/tag/type/gen2.ex
@@ -6,26 +6,28 @@ defmodule TM.Mercury.Tag.Type.Gen2 do
     epc_byte_len = bytes_for_bits(bit_len) - 4
     <<pc0, _pc1, tail :: binary>> = tail
 
-    epc_byte_len =
+    {epc_byte_len, tail} =
       if (pc0 &&& 0x02) == 0x02 do
-        <<pc2, _pc3, tail :: binary>> = tail
+        <<pc2, _pc3, rest :: binary>> = tail
         epc_byte_len = epc_byte_len - 2
 
           if (pc2 &&& 0x80) == 0x80 do
-            <<_pc4, _pc5, tail :: binary>> = tail
-            epc_byte_len - 2
+            <<_pc4, _pc5, rest :: binary>> = rest
+            {epc_byte_len - 2, rest}
           else
-            epc_byte_len
+            {epc_byte_len, rest}
           end
       else
-        epc_byte_len
+        {epc_byte_len, tail}
       end
 
-    <<epc :: size(epc_byte_len)-bytes, crc :: uint16, tail :: binary>> = tail
+    <<epc :: size(epc_byte_len)-bytes, crc :: 16, tail :: binary>> = tail
+
     result =
       result
       |> Keyword.put(:epc, epc)
       |> Keyword.put(:epc_crc, crc)
+
     {tail, result}
   end
 
