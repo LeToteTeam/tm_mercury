65c930d3a0c7bbced8ef8151334aea12fa628367
Jeff Smith <jeff@sohlsmith.com>
2018-03-20 14:36:21 -0500

    Handle response from read_sync more efficiently

--- lib/tm_mercury/read_async_task.ex
+++ lib/tm_mercury/read_async_task.ex
@@ -27,16 +27,8 @@ defmodule TM.Mercury.ReadAsyncTask do
 
   defp dispatch(%{status: :running} = state) do
     try do
-      case Reader.read_sync(state.reader, state.on, state.read_plan) do
-        {:ok, tags} when length(tags) == 0 -> state
-        {:ok, tags} when length(tags) > 0 ->
-          send(state.listener, {:tm_mercury, :tags, tags})
-          :ok = Reader.clear_tag_id_buffer(state.reader)
-          state
-        {:error, :timeout} ->
-          Logger.warn("Suspending asynchronous reads due to timeout")
-          %{state | status: :suspended}
-      end
+      Reader.read_sync(state.reader, state.on, state.read_plan)
+      |> handle_read_response(state)
     catch
       :exit, reason ->
         Logger.warn("Suspending asynchronous reads due to exit reason: #{inspect reason}")
@@ -46,4 +38,24 @@ defmodule TM.Mercury.ReadAsyncTask do
 
   # noop while suspended
   defp dispatch(%{status: :suspended} = state), do: state
+
+  defp handle_read_response({:ok, []}, state) do
+    state
+  end
+
+  defp handle_read_response({:ok, tags}, state) do
+    send(state.listener, {:tm_mercury, :tags, tags})
+    :ok = Reader.clear_tag_id_buffer(state.reader)
+    state
+  end
+
+  defp handle_read_response({:error, :timeout}, state) do
+    Logger.warn("Suspending asynchronous reads due to timeout")
+    %{state | status: :suspended}
+  end
+
+  defp handle_read_response(other, state) do
+    Logger.warn("Unexpected response during async dispatch to read_sync: #{inspect other}")
+    state
+  end
 end
