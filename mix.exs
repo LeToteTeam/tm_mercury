cd3e10bef4d630244d2203f176cd12164335b77c
Justin Schneck <jschneck@mac.com>
2019-01-21 10:39:05 -0500

    Clean up mix file and update deps

--- mix.exs
+++ mix.exs
@@ -2,25 +2,31 @@ defmodule TM.Mercury.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :tm_mercury,
-     version: "0.5.2",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description(),
-     package: package(),
-     deps: deps()]
+    [ 
+      app: :tm_mercury,
+      version: "0.5.2",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
-    [extra_applications: [:logger, :connection, :nerves_uart]]
+    [
+      extra_applications: [:logger]
+    ]
   end
 
   defp deps do
-    [{:connection, "~> 1.0.0"},
-     {:circuits_uart, "~> 1.3"},
-     {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
-     {:ex_doc, "~> 0.16.4", only: :dev, runtime: false}]
+    [
+      {:connection, "~> 1.0.0"},
+      {:circuits_uart, "~> 1.3"},
+      {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false}
+    ]
   end
 
   defp description do
@@ -30,7 +36,9 @@ defmodule TM.Mercury.Mixfile do
   end
 
   defp package do
-     licenses: ["Apache 2.0"],
-     links: %{"Github" => "https://github.com/letoteteam/tm_mercury"}]
+    [
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/letoteteam/tm_mercury"}
+    ]
   end
 end
